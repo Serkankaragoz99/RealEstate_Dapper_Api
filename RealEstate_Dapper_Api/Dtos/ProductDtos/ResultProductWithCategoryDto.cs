632996b8119c95dfd2ed2b93e07ae436b335dccb
Serkankaragoz99 <103015554+Serkankaragoz99@users.noreply.github.com>
2023-09-23 01:43:24 +0300

    Add project files.

---
+++ RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.ProductDtos
+{
+    public class ResultProductWithCategoryDto
+    {
+        public int ProductID { get; set; }
+        public string ProductTitle { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductCity { get; set; }
+        public string ProductDistrict { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductCoverimage { get; set; }
+        public string Type { get; set; }
+        public string ProductAddress { get; set; }
+    }
+}
